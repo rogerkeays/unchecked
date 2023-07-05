e50d0512bdb71660448df277bc0f5d6d08b662ae
guybrush <realguybrush@vivaldi.net>
2023-07-05 15:58:46 +0700

    add instructions from old README

--- Functions.java
+++ Functions.java
@@ -3,7 +3,125 @@ package jamaica.unchecked;
 import java.util.function.*;
 import java.util.concurrent.Callable;
 
-// functional wrappers which rethrow all exceptions as unchecked exceptions
+/**
+ * Functional wrappers which rethrow all exceptions as unchecked exceptions,
+ * and a utility method to rethrow checked exceptions as unchecked exceptions
+ * from your procedural code.
+ *
+ * This is largely superseded by the compiler plugin, but is left here 
+ * because it is still useful for cases when you can't use the plugin.
+ *
+ * ## Functional Examples:
+ * 
+ * Before:
+ * 
+ *     List.of("LICENSE", "README.md", "unchecked.java").stream()
+ *         .map(file -> {
+ *             try {
+ *                 return(file + ": " + Files.lines(Paths.get(file)).count());
+ *             } catch (IOException e) {
+ *                 throw new RuntimeException(e); // java made me do it
+ *             }
+ *         })
+ *         .toList();
+ * 
+ * After (exceptions will propagate up the call stack):
+ * 
+ *     List.of("LICENSE", "README.md", "unchecked.java").stream()
+ *         .map(unchecked(file -> file + ": " + Files.lines(Paths.get(file)).count()))
+ *         .toList();
+ * 
+ * You can use `uc` instead of `unchecked` if you prefer:
+ * 
+ *     List.of("LICENSE", "README.md", "unchecked.java").stream()
+ *         .map(uc(file -> file + ": " + Files.lines(Paths.get(file)).count()))
+ *         .toList();
+ * 
+ * Note, consumer functions must use `uncheckedconsumer` or `ucc`:
+ * 
+ *     List.of("LICENSE", "README.md", "unchecked.java")
+ *         .forEach(uncheckedconsumer(file -> 
+ *             System.out.println(file + ": " + Files.lines(Paths.get(file)).count())));
+ * 
+ *     List.of("LICENSE", "README.md", "unchecked.java")
+ *         .forEach(ucc(file -> 
+ *             System.out.println(file + ": " + Files.lines(Paths.get(file)).count())));
+ * 
+ * ## Procedural Examples
+ * 
+ * When you can't handle a checked exception, a common practise is to rethrow it
+ * as a RuntimeException:
+ * 
+ *     public static void rm() {
+ *         try {
+ *             Files.delete(Paths.get("unchecked.kt"));
+ *         } catch (IOException e) {
+ *             throw new RuntimeException(e);
+ *         }
+ *     }
+ * 
+ *     |  Exception java.lang.RuntimeException: java.nio.file.NoSuchFileException: unchecked.kt
+ *     |        at rm (#22:5)
+ *     |        at (#23:1)
+ *     |  Caused by: java.nio.file.NoSuchFileException: unchecked.kt
+ *     |        at UnixException.translateToIOException (UnixException.java:92)
+ *     |        at UnixException.rethrowAsIOException (UnixException.java:106)
+ *     |        at UnixException.rethrowAsIOException (UnixException.java:111)
+ *     |        at UnixFileSystemProvider.implDelete (UnixFileSystemProvider.java:248)
+ *     |        at AbstractFileSystemProvider.delete (AbstractFileSystemProvider.java:105)
+ *     |        at Files.delete (Files.java:1152)
+ *     |        at rm (#22:3)
+ *     |        ...
+ * 
+ * With `unchecked`, you can throw checked exceptions without wrapping them or
+ * declaring them in the method signature:
+ * 
+ *     public static void rm() {
+ *         try {
+ *             Files.delete(Paths.get("unchecked.kt"));
+ *         } catch (IOException e) {
+ *             throw unchecked(e);
+ *         }
+ *     }
+ * 
+ *     |  Exception java.nio.file.NoSuchFileException: unchecked.kt
+ *     |        at UnixException.translateToIOException (UnixException.java:92)
+ *     |        at UnixException.rethrowAsIOException (UnixException.java:106)
+ *     |        at UnixException.rethrowAsIOException (UnixException.java:111)
+ *     |        at UnixFileSystemProvider.implDelete (UnixFileSystemProvider.java:248)
+ *     |        at AbstractFileSystemProvider.delete (AbstractFileSystemProvider.java:105)
+ *     |        at Files.delete (Files.java:1152)
+ *     |        at rm (#20:3)
+ *     |        at (#21:1)
+ * 
+ * This is possible because of Java's runtime type erasure. See the code for
+ * implementation details.
+ * 
+ * ## Installation
+ * 
+ * `unchecked` is a single java source file that you can drop into your project.
+ * 
+ * Copy the java source to your project:
+ * 
+ *     mkdir -p src/jamaica/unchecked
+ *     cd src/jamaica/unchecked
+ *     wget https://github.com/rogerkeays/unchecked/raw/main/Functions.java
+ * 
+ * Finally, import to use in your code:
+ * 
+ *     import static jamaica.unchecked.Functions.*;
+ * 
+ * ## Testing
+ * 
+ * Tests are run by executing the `main` method of this class . No output means
+ * the tests ran successfully.  If you want to run the tests from your own
+ * build system, make sure assertions are enabled with the `java -ea` switch.
+ * 
+ * ## Related Resources
+ * 
+ *  - Lombok @SneakyThrows: https://projectlombok.org/features/SneakyThrows
+ *  - Function type inference discussion on Stack Overflow: https://stackoverflow.com/questions/71276582/why-does-java-type-inference-fail-to-distinguish-between-function-and-consumer
+ */
 public class Functions {
 
     // testable specification
