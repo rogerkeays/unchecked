2001794858ca16164da2f15b272f6a69c8f4985e
guybrush <realguybrush@vivaldi.net>
2023-07-06 11:57:59 +0700

    reformat code for better test output

--- TestErrors.java
+++ TestErrors.java
@@ -2,11 +2,9 @@
 public class TestErrors {
 
     public static void main(String [] args) {
-    
-        // unreachable catch
         try {
            throw new Exception();
         } catch (Exception e) {
-        } catch (Exception e) {}
+        } catch (Exception e) {} // already caught
     }
 }
