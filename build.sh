fc6651d80435338b20e68687cdddde43e698b5e3
guybrush <realguybrush@vivaldi.net>
2023-07-13 18:13:09 +0700

    implement build targets

--- build.sh
+++ build.sh
@@ -1,5 +1,20 @@
 #!/bin/sh
 
+USAGE="
+usage: $0 action
+
+valid actions:
+  clean
+  compile
+  jar
+  test-warnings [-all] run tests expected to show warnings
+  test-errors [-all]   run tests expected to show errors
+  test [-all]          run all tests
+  install              install locally with maven
+  bundle               bundle for release to central
+
+use -all to test against all JDKs in \$HOME/tools
+"
 # build configuration
 GROUP=jamaica
 NAME=unchecked
@@ -19,42 +34,46 @@ TEST_CLASSPATH=$JAR
 
 # directories containing jdks to test against, separated by spaces
 JDKS="$JAVA_HOME"
-[ "$1" = "all" ] && JDKS="$HOME/tools/jdk-*"
+[ "$2" = "-all" ] && JDKS="$HOME/tools/jdk-*"
 
-# compile and build jar
-# note: -source 8 is required to import com.sun.tools.javac.*
-echo "===== BUILDING ====="
-echo $JAVA_HOME
+# build code starts here
+[ ! $1 ] && echo "$USAGE" && exit 1;
+
+echo ">>> clean"
 [ -d target ] && rm -r target
-mkdir -p target/META-INF/services
-echo "$PACKAGE.$CLASSNAME" > target/META-INF/services/com.sun.source.util.Plugin
+[ $1 = "clean" ] && exit 0
+
+# note: -source 8 is required to import com.sun.tools.javac.*
+echo ">>> compile ($JAVA_HOME)"
 $JAVA_HOME/bin/javac -Xlint:unchecked -nowarn -source 8 -target $TARGET -d target $CLASSNAME.java
 [ $? -eq 0 ] || exit 1
-cd target; $JAVA_HOME/bin/jar --create --file ../$JAR *; cd ..
+[ $1 = "compile" ] && exit 0
 
-# test against all jdks
-echo "\n===== TESTING ====="
-echo "----- press enter to run warning test cases"; read x
+echo ">>> jar"
+mkdir -p target/META-INF/services
+echo "$PACKAGE.$CLASSNAME" > target/META-INF/services/com.sun.source.util.Plugin
+cd target; $JAVA_HOME/bin/jar --create --file ../$JAR *; cd ..
+[ $1 = "jar" ] && exit 0
+
 for JDK in $JDKS; do
-    echo $JDK
+    echo ">>> test-warnings ($JDK)"
     "$JDK"/bin/javac -cp $TEST_CLASSPATH -d target "$PLUGIN" $TEST_OPTS TestWarnings.java
     [ $? -eq 0 ] || exit 1
     "$JDK"/bin/java -cp target -enableassertions TestWarnings
     [ $? -eq 0 ] || exit 1
 done
-echo "\n----- press enter to run attr error test cases"; read x
+[ $1 = "test-warnings" ] && exit 0
+
 for JDK in $JDKS; do
-    echo $JDK
+    echo ">>> test-errors ($JDK)"
     "$JDK"/bin/javac -cp $TEST_CLASSPATH -d target "$PLUGIN" $TEST_OPTS TestAttrErrors.java
-done
-echo "\n----- press enter to run flow error test cases"; read x
-for JDK in $JDKS; do
-    echo $JDK
     "$JDK"/bin/javac -cp $TEST_CLASSPATH -d target "$PLUGIN" $TEST_OPTS TestFlowErrors.java
 done
+[ $1 = "test-errors" ] && exit 0
+[ $1 = "test" ] && exit 0
 
-# install using maven
-echo "\n===== INSTALLING WITH MAVEN ====="
-echo "----- press enter to install using maven"; read x
+echo ">>> install"
 mvn install:install-file -DgroupId=$GROUP -DartifactId=$NAME -Dversion=$VERSION -Dpackaging=jar -Dfile=$JAR
+[ $? -eq 0 ] || exit 1
+[ $1 = "test-errors" ] && exit 0
 
